b376b36805f33add216840d7acbef6cb8d02988e
cedmondson98311 <cedmondson98311@gmail.com>
2017-04-15 17:51:09 -0700

    fixed config

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/blog';
 exports.PORT = process.env.PORT || 8080;
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://cory:1101762@ds161190.mlab.com:61190/workingblog';
